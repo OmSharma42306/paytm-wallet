970abc60208805b0c663d96a636a319177e66e9a
Om Sharma <omsharma.83173@gmail.com>
2024-11-27 03:54:40 +0530

    Removed-Landing-Page-in-root.tsx-to-another-component

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,17 +1,12 @@
 "use client"
 import { getServerSession } from "next-auth";
 import {redirect} from "next/navigation"
-import { authOptions } from "./lib/auth";
 import { AppbarClient } from "./AppbarClient";
-export default async function Page(){
-    const session = await getServerSession(authOptions);
-    if(session?.user){
-      redirect('/dashboard')
-    }else{
-      redirect('/api/auth/signin')
-    }
-
-  return (<div>
+import { authOptions } from "./lib/auth";
+export default async function Page(): JSX.Element {
+  
+  return (
+   <div>
     <AppbarClient/>
    </div>
   );
