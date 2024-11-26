b13fe1c23127ee512d14d1cf7fdd49d389b7f123
Om Sharma <omsharma.83173@gmail.com>
2024-11-27 03:45:57 +0530

    Added-Landing-Page-For-redirect

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,9 +1,17 @@
 "use client"
+import { getServerSession } from "next-auth";
+import {redirect} from "next/navigation"
+import { authOptions } from "./lib/auth";
 import { AppbarClient } from "./AppbarClient";
-export default function Page(): JSX.Element {
-  
-  return (
-   <div>
+export default async function Page(){
+    const session = await getServerSession(authOptions);
+    if(session?.user){
+      redirect('/dashboard')
+    }else{
+      redirect('/api/auth/signin')
+    }
+
+  return (<div>
     <AppbarClient/>
    </div>
   );
